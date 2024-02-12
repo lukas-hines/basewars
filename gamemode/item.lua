7b9f678c3e0abe6cde702d695eea1974e446c813
lukas <grimmygumdrops@yahoo.com>
2024-02-12 07:38:53 +0000

    updated item

--- gamemode/item.lua
+++ gamemode/item.lua
@@ -1,22 +1,30 @@
 local Item = {}
 Item.__index = Item
 
-function Item.new(name, description, value, sellable)
+function Item.new(name, description, value, icon, model, sellable, dropable)
     local self = setmetatable({}, Item)
     self.id = nil
     self.name = name or "no name"
     self.description = description or "no description"
-    self.value = value
+    self.value = value or 0
+    self.icon = icon or nil
+    self.model = model or nil
+    self.sellable = sellable or false 
+    self.dropable = dropable or false
 
     return self
 end
 
-function Item:SetId(id)
-    self.id = id
+function Item:Drop(pos)
+    
 end
 
 function Item:use()
     print("[BW] item ".. self.name .. " id: " .. self.id .. " did nothing intresting")
+end
+
+function Item:SetId(id)
+    self.id = id
 end
 
 function Item:__tostring()
